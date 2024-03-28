f9805028ab23a2f746c549f45baea4d48dcad984
Nari <byenar1isme@gmail.com>
2024-03-28 15:52:17 -0700

    fetch items

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+
+type Rating = {
+  count: number;
+  rate: number;
+};
+
+type Item = {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: Rating;
+  title: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [allItems, setAllItems] = useState<Item[]>([]);
+  const url = 'https://fakestoreapi.com/products';
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setAllItems(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container flex m-5">
+        <ul className="-my-6 divide-y divide-gray-200 m-5">
+          {allItems.length === 0
+            ? ''
+            : allItems.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className=" my-2 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-cyan-200  dark:bg-white dark:hover:bg-cyan-200"
+                  >
+                    <img
+                      className="object w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                      src={item.image}
+                      alt=""
+                    />
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                        {item.title}
+                      </h5>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-black">
+                        {item.description}
+                      </p>
+                      <button className="border bg-blue-600 rounded text-white max-w-20 text-lg">
+                        Add
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+        </ul>
+        <ul className="m-5 fixed right-0 items-center border-gray-200rounded-lg shadow w-60">
+          <li>test1</li>
+          <li>test2</li>
+          <li>test3</li>
+          <li>test4</li>
+          <li>test4444444444444444444</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
